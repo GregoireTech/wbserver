695e07d61e0842515d2939de126122fe076ae399
Gregoire888 <greg.claise@gmail.com>
2019-01-26 21:47:22 +0100

    with endpoints

---
+++ config/endpoints.js
@@ -0,0 +1,5 @@
+
+exports.prod = "https://wboardserver.herokuapp.com/";
+exports.dev = "http://localhost:3000";
+exports.email_user = 'XrzgeVJdQu6wvBVR3lWuUw';
+exports.email_key = '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';
